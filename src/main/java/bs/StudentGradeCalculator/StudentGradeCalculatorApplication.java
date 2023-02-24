db0c913d5a7dc6b500f45ed7f54d9f0f5907ac9b
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2023-02-24 21:22:38 +0000

    Initial Commit

---
+++ src/main/java/bs/StudentGradeCalculator/StudentGradeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package bs.StudentGradeCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bs/StudentGradeCalculator/StudentGradeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package bs.StudentGradeCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
